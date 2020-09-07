5c0a19bde23b160fd820d5f87c119a842a58d2b5
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-09-07 23:15:18 +1000

    Finished tutorial: Creating and combining views
    https://developer.apple.com/tutorials/swiftui/creating-and-combining-views

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Timothy Hoiberg on 7/9/20.
+//  Copyright © 2020 Timothy Hoiberg. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
