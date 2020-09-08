a0801e91f0a221298bb8909b15ca82ece4b594f1
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-09-09 01:45:11 +1000

    Add tutorial for drawing paths and shapes
    https://developer.apple.com/tutorials/swiftui/drawing-paths-and-shapes

---
+++ Landmarks/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeView.swift
+//  Landmarks
+//
+//  Created by Timothy Hoiberg on 9/9/20.
+//  Copyright © 2020 Timothy Hoiberg. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+        .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
